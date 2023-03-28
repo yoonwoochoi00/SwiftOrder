bde35225d220be05f6cc5a5a2fd03f9da5ad4942
yunuchoi621 <91050411+yunuchoi621@users.noreply.github.com>
2023-03-28 22:49:02 +1300

    Fix database connection to using configuration

--- SwiftOrder-Server/Program.cs
+++ SwiftOrder-Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SwiftOrder_Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SwiftOrderDbContext>(options => options.UseSqlite(builder.Configuration["WebAPIConnection"]));
 
 var app = builder.Build();
 
